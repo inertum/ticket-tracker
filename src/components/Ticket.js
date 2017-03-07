d8126e63397d242b39d23d98a9f6e42d3d58e2a9
Ollipekka Jalonen <ollipekka.jalonen@gmail.com>
2017-03-07 13:00:00 +0200

    removed placeholder inline css - css to be updated

--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -67,7 +67,7 @@ class Ticket extends Component {
       <section>
         <AppBar title="ADD A NEW TICKET" />
         <div style={{ flex: 1, overflowY: 'auto', padding: '1.8rem' }}>
-          <div style={{ display: 'inline-block', overflowY: 'auto', width: '20%', verticalAlign: 'top' }}>
+          <div>
             <p>Choose type</p>
             <RadioGroup style={{ display: 'inline-block' }} name="type" value={this.state.type} onChange={this.handleTypeChange}>
               <RadioButton style={{ display: 'inline-block', padding: '0.5em' }} label="Project" value="project" />
@@ -75,14 +75,14 @@ class Ticket extends Component {
               <RadioButton style={{ display: 'inline-block', padding: '0.5em' }} label="Task" value="task" />
             </RadioGroup>
           </div>
-          <div style={{ display: 'inline-block', overflowY: 'auto', width: '80%' }}>
-            <div style={{ display: 'inline-block', overflowY: 'auto', width: '33%' }}>
+          <div>
+            <div>
               <DatePicker icon="view_day" hint="Choose a date" label="Date" name="Date" value={this.state.date} onChange={this.handleChange.bind(this, 'date')} />
             </div>
-            <div style={{ display: 'inline-block', overflowY: 'auto', width: '33%' }}>
+            <div>
               <TimePicker icon="schedule" hint="Choose start time" label="Start Time" name="startTime" value={this.state.startTime} onChange={this.handleChange.bind(this, 'startTime')} size="6" />
             </div>
-            <div style={{ display: 'inline-block', overflowY: 'auto', width: '33%' }}>
+            <div>
               <TimePicker icon="schedule" hint="Choose end time" label="End Time" name="endTime" value={this.state.endTime} onChange={this.handleChange.bind(this, 'endTime')} />
             </div>
             <Autocomplete
@@ -98,7 +98,7 @@ class Ticket extends Component {
               source={customerSource}
               value={this.state.customer}
             />
-            <div style={{ display: 'inline-block', overflowY: 'auto', width: '50%' }}>
+            <div>
               <Autocomplete
                 icon="work"
                 title="Project"
@@ -113,7 +113,7 @@ class Ticket extends Component {
                 onChange={this.handleChange.bind(this, 'project')}
               />
             </div>
-            <div style={{ display: 'inline-block', overflowY: 'auto', width: '50%' }}>
+            <div>
               <Autocomplete
                 icon="search"
                 title="Reference"
